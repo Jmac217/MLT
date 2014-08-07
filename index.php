c10ff408d8dbf843800ccc9b9a7053eda1099224
Jmac217 <jordanelder10@gmail.com>
2014-08-07 15:40:27 -0500

    rearranging index for db restructure; added the new json db file

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
 				<!-- Checkboxes -->
 				
 				<!-- HMDA Data / Gov Monitoring -->
+				<!--
 				<span id='govMonitoring'>
 					HMDA Data / Government Monitoring?
 					<span id='govMonitoringCheckbox'>
@@ -45,6 +46,7 @@
 						Edit
 					</span>
 				</span>
+				-->
 				
 				<!-- Loan Accepted is being replaced by Conditionally Approved -->
 				<!-- Conditionally Approved -->
@@ -62,202 +64,24 @@
 					<span id='conditionallyApprovedAnswerEdit'>
 						Edit
 					</span>
+					<!-- has date -->
 				</span>
 				
-				<!-- was early disclosure delivered -->
-				<span id='earlyDisclosure'>
-					Was early disclosure delivered
-					<span id='earlyDisclosureCheckbox'>
-						Yes <input id='wasEarlyDisclosureYes' type='radio' />
-						No <input id='wasEarlyDisclosureNo' type='radio' />
+				<!-- Appraisal Ordered -->
+				<span id='appraisalOrdered'>
+					Appraisal Ordered
+					<span id='appraisalOrderedCheckbox'>
+						Yes <input id='wasAppraisalOrdered' type='checkbox' />
 					</span>
 				</span>
-				<span id='earlyDisclosureAnswer'>
-					<span id='earlyDisclosureAnswerYesNo'>
+				<span id='appraisalOrderedAnswer'>
+					<span id='appraisalOrderedAnswerBool'>
 						Yes/No
 					</span>
-					<span id='earlyDisclosureAnswerEdit'>
+					<span id='appraisalOrderedAnswerEdit'>
 						Edit
 					</span>
-				</span>
-
-				<span id='loanStatus'>
-					Loan Status : 
-					<span id='loanStatusCheckbox'>
-						Processing/Underwriting <input id='loanStatusProcessing' type='radio' />
-						Final Approved <input id='loanStatusApproved' type='radio' />
-						Closing Prep <input id='loanStatusClosing' type='radio' />
-					</span>
-				</span>
-				<span id='loanStatusAnswer'>
-					<span id='loanStatusAnswerYesNo'>
-						Yes/No
-					</span>
-					<span id='loanStatusAnswerEdit'>
-						Edit
-					</span>
-				</span>
-
-				
-				<!-- title work ordered -->
-				<!-- this form is special, it requires 3 selectors -->
-				<span id='titleOrdered'>
-					Title Work Ordered
-					<span id='titleOrderedCheckbox'>
-						No <input id='titleOrderedNo' type='radio' />
-						By Bank <input id='titleOrderedBank' type='radio' />
-						By Realtor <input id='titleOrderedRealtor' type='radio' />
-					</span>
-				</span>
-				<span id='titleOrderedAnswer'>
-					<span id='titleOrderedAnswerYesNo'>
-						Yes/No
-					</span>
-					<span id='titleOrderedAnswerEdit'>
-						Edit
-					</span>
-				</span>
-				
-				<!-- Title Work Approved -->
-				<span id='titleApproved'>
-					Title Work Approved
-					<span id='titleApprovedCheckbox'>
-						Yes <input id='titleApprovedYes' type='radio' />
-						No <input id='titleApprovedNo' type='radio' />
-					</span>
-				</span>
-				<span id='titleApprovedAnswer'>
-					<span id='titleApprovedAnswerYesNo'>
-						Yes/No
-					</span>
-					<span id='titleApprovedAnswerEdit'>
-						Edit
-					</span>
-				</span>
-
-					<!-- Title Ordered Date -->
-					<div id='loanStatusDate'>
-						<span id='loanStatusDateTitle'>Loan Status : </span>
-						<span id='loanStatusDateWrapper'>
-						<select id ='loanStatusMonth' name="month">
-							<option value="01">January
-							<option value="02">February
-							<option value="03">March
-							<option value="04">April
-							<option value="05">May
-							<option value="06">June
-							<option value="07">July
-							<option value="08">August
-							<option value="09">September
-							<option value="10">October
-							<option value="11">November
-							<option value="12">December
-						</select>
-						<select id='loanStatusDay' name="day">
-							<option value="01">1
-							<option value="02">2
-							<option value="03">3
-							<option value="04">4
-							<option value="05">5
-							<option value="06">6
-							<option value="07">7
-							<option value="08">8
-							<option value="09">9
-							<option value="10">10
-							<option value="11">11
-							<option value="12">12
-							<option value="13">13
-							<option value="14">14
-							<option value="15">15
-							<option value="16">16
-							<option value="17">17
-							<option value="18">18
-							<option value="19">19
-							<option value="20">20
-							<option value="21">21
-							<option value="22">22
-							<option value="23">23
-							<option value="24">24
-							<option value="25">25
-							<option value="26">26
-							<option value="27">27
-							<option value="28">28
-							<option value="29">29
-							<option value="30">30
-							<option value="31">31
-						</select>
-						<select id='loanStatusYear' name="year">
-							<option value="2014">2014
-							<option value="2015">2015
-						</select>
-						</span>
-						<input type='input' value='Loan Status Comments' id='loanStatusComment' alt='Loan Status Comments'></input>
-					</div>
-					<span id='loanStatusDateInfo'><span id='loanStatusDateInfoDate'></span><a id='loanStatusDateInfoLink'>Edit</a></span>
-					
-
-				<br /><br />
-				
-				<table>
-					<!-- Date Approved ( Accepted Date ) -->
-					<div id='acceptedDate'>
-						<span id='acceptedDateTitle'>Date Approved : </span>
-						<span id='acceptedDateWrapper'>
-						<select id ='acceptedMonth' name="month">
-							<option value="01">January
-							<option value="02">February
-							<option value="03">March
-							<option value="04">April
-							<option value="05">May
-							<option value="06">June
-							<option value="07">July
-							<option value="08">August
-							<option value="09">September
-							<option value="10">October
-							<option value="11">November
-							<option value="12">December
-						</select>
-						<select id='acceptedDay' name="day">
-							<option value="01">1
-							<option value="02">2
-							<option value="03">3
-							<option value="04">4
-							<option value="05">5
-							<option value="06">6
-							<option value="07">7
-							<option value="08">8
-							<option value="09">9
-							<option value="10">10
-							<option value="11">11
-							<option value="12">12
-							<option value="13">13
-							<option value="14">14
-							<option value="15">15
-							<option value="16">16
-							<option value="17">17
-							<option value="18">18
-							<option value="19">19
-							<option value="20">20
-							<option value="21">21
-							<option value="22">22
-							<option value="23">23
-							<option value="24">24
-							<option value="25">25
-							<option value="26">26
-							<option value="27">27
-							<option value="28">28
-							<option value="29">29
-							<option value="30">30
-							<option value="31">31
-						</select>
-						<select id='acceptedYear' name="year">
-							<option value="2014">2014
-							<option value="2015">2015
-						</select>
-						</span>
-					</div>
-					<span id='acceptedDateInfo'><span id='acceptedDateInfoDate'></span><a id='acceptedDateInfoLink'>Edit</a></span>
-					
+					<!-- has date -->
 					<!-- Appraisal Ordered Date -->
 					<div id='appraisalOrderedDate'>
 					  <span id='appraisalOrderedDateTitle'>Appraisal Ordered : </span>
@@ -317,6 +141,208 @@
 						<!--<input type='input' value='Appraisal Ordered Comments' id='appraisalOrderedComment' alt='Appraisal Ordered Comments'></input>-->
 					</div>
 					<span id='appraisalOrderedDateInfo'><span id='appraisalOrderedDateInfoDate'></span><a id='appraisalOrderedDateInfoLink'>Edit</a></span>
+				</span>
+				
+				<!-- was early disclosure delivered -->
+				<!--
+				<span id='earlyDisclosure'>
+					Was early disclosure delivered
+					<span id='earlyDisclosureCheckbox'>
+						Yes <input id='wasEarlyDisclosureYes' type='radio' />
+						No <input id='wasEarlyDisclosureNo' type='radio' />
+					</span>
+				</span>
+				<span id='earlyDisclosureAnswer'>
+					<span id='earlyDisclosureAnswerYesNo'>
+						Yes/No
+					</span>
+					<span id='earlyDisclosureAnswerEdit'>
+						Edit
+					</span>
+				</span>
+				-->
+
+				<span id='loanStatus'>
+					Loan Status : 
+					<span id='loanStatusCheckbox'>
+						Processing/Underwriting <input id='loanStatusProcessing' type='radio' />
+						Awaiting Response <input id='loanStatusApproved' type='radio' />
+						Closing Prep <input id='loanStatusClosing' type='radio' />
+					</span>
+				</span>
+				<span id='loanStatusAnswer'>
+					<span id='loanStatusAnswerYesNo'>
+						Yes/No
+					</span>
+					<span id='loanStatusAnswerEdit'>
+						Edit
+					</span>
+					<!-- has date -->
+					<!-- loan status date -->
+					<div id='loanStatusDate'>
+						<span id='loanStatusDateTitle'>Loan Status : </span>
+						<span id='loanStatusDateWrapper'>
+						<select id ='loanStatusMonth' name="month">
+							<option value="01">January
+							<option value="02">February
+							<option value="03">March
+							<option value="04">April
+							<option value="05">May
+							<option value="06">June
+							<option value="07">July
+							<option value="08">August
+							<option value="09">September
+							<option value="10">October
+							<option value="11">November
+							<option value="12">December
+						</select>
+						<select id='loanStatusDay' name="day">
+							<option value="01">1
+							<option value="02">2
+							<option value="03">3
+							<option value="04">4
+							<option value="05">5
+							<option value="06">6
+							<option value="07">7
+							<option value="08">8
+							<option value="09">9
+							<option value="10">10
+							<option value="11">11
+							<option value="12">12
+							<option value="13">13
+							<option value="14">14
+							<option value="15">15
+							<option value="16">16
+							<option value="17">17
+							<option value="18">18
+							<option value="19">19
+							<option value="20">20
+							<option value="21">21
+							<option value="22">22
+							<option value="23">23
+							<option value="24">24
+							<option value="25">25
+							<option value="26">26
+							<option value="27">27
+							<option value="28">28
+							<option value="29">29
+							<option value="30">30
+							<option value="31">31
+						</select>
+						<select id='loanStatusYear' name="year">
+							<option value="2014">2014
+							<option value="2015">2015
+						</select>
+						</span>
+						<input type='input' value='Loan Status Comments' id='loanStatusComment' alt='Loan Status Comments'></input>
+					</div>
+					<span id='loanStatusDateInfo'><span id='loanStatusDateInfoDate'></span><a id='loanStatusDateInfoLink'>Edit</a></span>
+				</span>
+
+				
+				<!-- title work ordered -->
+				<!-- this form is special, it requires 3 selectors -->
+				<span id='titleOrdered'>
+					Title Work Ordered
+					<span id='titleOrderedCheckbox'>
+						No <input id='titleOrderedNo' type='radio' />
+						By Bank <input id='titleOrderedBank' type='radio' />
+						By Realtor <input id='titleOrderedRealtor' type='radio' />
+					</span>
+				</span>
+				<span id='titleOrderedAnswer'>
+					<span id='titleOrderedAnswerYesNo'>
+						Yes/No
+					</span>
+					<span id='titleOrderedAnswerEdit'>
+						Edit
+					</span>
+					<!-- has date -->
+				</span>
+				
+				<!-- Title Work Approved -->
+				<span id='titleApproved'>
+					Title Work Approved
+					<span id='titleApprovedCheckbox'>
+						Yes <input id='titleApprovedYes' type='radio' />
+						No <input id='titleApprovedNo' type='radio' />
+					</span>
+				</span>
+				<span id='titleApprovedAnswer'>
+					<span id='titleApprovedAnswerYesNo'>
+						Yes/No
+					</span>
+					<span id='titleApprovedAnswerEdit'>
+						Edit
+					</span>
+					<!-- has date -->
+				</span>					
+
+				<br /><br />
+				
+				<table>
+					<!-- Date Approved ( Accepted Date ) || Unused -> replaced with Conditionally Approved Date-->
+					<!--
+					<div id='acceptedDate'>
+						<span id='acceptedDateTitle'>Date Approved : </span>
+						<span id='acceptedDateWrapper'>
+						<select id ='acceptedMonth' name="month">
+							<option value="01">January
+							<option value="02">February
+							<option value="03">March
+							<option value="04">April
+							<option value="05">May
+							<option value="06">June
+							<option value="07">July
+							<option value="08">August
+							<option value="09">September
+							<option value="10">October
+							<option value="11">November
+							<option value="12">December
+						</select>
+						<select id='acceptedDay' name="day">
+							<option value="01">1
+							<option value="02">2
+							<option value="03">3
+							<option value="04">4
+							<option value="05">5
+							<option value="06">6
+							<option value="07">7
+							<option value="08">8
+							<option value="09">9
+							<option value="10">10
+							<option value="11">11
+							<option value="12">12
+							<option value="13">13
+							<option value="14">14
+							<option value="15">15
+							<option value="16">16
+							<option value="17">17
+							<option value="18">18
+							<option value="19">19
+							<option value="20">20
+							<option value="21">21
+							<option value="22">22
+							<option value="23">23
+							<option value="24">24
+							<option value="25">25
+							<option value="26">26
+							<option value="27">27
+							<option value="28">28
+							<option value="29">29
+							<option value="30">30
+							<option value="31">31
+						</select>
+						<select id='acceptedYear' name="year">
+							<option value="2014">2014
+							<option value="2015">2015
+						</select>
+						</span>
+					</div>
+					<span id='acceptedDateInfo'><span id='acceptedDateInfoDate'></span><a id='acceptedDateInfoLink'>Edit</a></span>
+					-->
+					
+
 					
 					<!-- Appraisal Approved Date -->
 					<div id='appraisalApprovedDate'>
